36e19f5f6a897ef71a63fba57b7e13842f50f88d
crusadercosmo <gaganparashar127@gmail.com>
2023-07-18 19:28:54 +0530

    initial upload of mernapp

---
+++ mernapp/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        hi hello
+    </div>
+
+  )
+}
+
+export default Navbar 
---
+++ mernapp/src/screens/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div>body</div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
