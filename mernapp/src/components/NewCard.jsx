d9f624a31746d25aba8c2bf6974a020fc9072589
crusadercosmo <gaganparashar127@gmail.com>
2023-08-10 11:00:06 +0530

    trial

--- mernapp/src/components/NewCard.jsx
+++ mernapp/src/components/NewCard.jsx
@@ -2,28 +2,45 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 
 const NewCard = (props) => {
+  const { options, imgSrc, foodName, description } = props;
+
+  // Extracting the keys from the options object and storing them in the priceOptions array.
+  const priceOptions = Object.keys(options);
+
   return (
-    <div className="mb-3">
-      <Card style={{ width: "16rem", maxHeight: "400px" }}>
-        <Card.Img variant="top" src={props.imgSrc} />
+    <div className="mb-3 ">
+      <Card style={{ width: "16rem", height: "410px" }}>
+        <Card.Img
+          variant="top"
+          src={imgSrc}
+          style={{ height: "180px", objectFit: "fill" }}
+        />
         <Card.Body>
-          <Card.Title>{props.foodName}</Card.Title>
-          <Card.Text style={{ fontSize: "13px", color: "#E1E1D9" }}>
-            {props.description}
+          <Card.Title>
+            {" "}
+            <b>{foodName}</b>{" "}
+          </Card.Title>
+          <Card.Text style={{ fontSize: "12px", color: "#E1E1D9" }}>
+            {description}
           </Card.Text>
-          <div className="container w-100">
+
+          <div className="container w-100 ">
+            {/* Array.from(...): Turn this empty list[Array(6)] into a real array and do something with each slot. */}
             <select className="m-2 h-100 rounded bg-dark text-bg-dark">
-              {Array.from(Array(6), (e, i) => {
-                return (
-                  <option key={i + 1} value={i + 1}>
-                    {i + 1}
-                  </option>
-                );
-              })}
+              {Array.from(Array(6), (value, i) => (
+                <option key={i + 1} value={i + 1}>
+                  {i + 1}
+                </option>
+              ))}
             </select>
+
+            {/* Dropdown for selecting price options */}
             <select className="m-2 h-100 rounded bg-dark text-bg-dark">
-              <option value="half">Half</option>
-              <option value="full">Full</option>
+              {priceOptions.map((data) => (
+                <option key={data} value={data}>
+                  {data}
+                </option>
+              ))}
             </select>
           </div>
           {/* <Button variant="primary">Go somewhere</Button> */}
--- mernapp/src/screens/Home.jsx
+++ mernapp/src/screens/Home.jsx
@@ -2,15 +2,19 @@ import React, { useState, useEffect } from "react";
 import NewNavbar from "../components/NewNavbar";
 import NewCard from "../components/NewCard";
 import Footer from "../components/Footer";
-import ControlledCarousel from "../components/ControlledCarousel";
+import Form from "react-bootstrap/Form";
+import Carousel from "react-bootstrap/Carousel";
+import CarouselImageGenerator from "../components/CarouselImageGenerator";
 
 const Home = () => {
+  const [search, setSearch] = useState("");
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const loadData = async () => {
     try {
-      let response = await fetch("http://localhost:5000/api/fooddata", {
+      const response = await fetch("http://localhost:5000/api/fooddata", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -18,15 +22,16 @@ const Home = () => {
       });
 
       if (response.ok) {
-        let data = await response.json();
-        console.log(data);
-        setFoodItem(data[0]);
-        setFoodCat(data[1]);
+        const [foodItems, categories] = await response.json();
+        setFoodItem(foodItems);
+        setFoodCat(categories);
       } else {
         console.error("Failed to fetch data");
       }
     } catch (error) {
       console.error("Error fetching data:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -36,50 +41,111 @@ const Home = () => {
 
   return (
     <div>
-      <div>
-        <NewNavbar />
-      </div>
-      <div>
-        <ControlledCarousel />
-      </div>
+      <NewNavbar />
+      <Carousel fade style={{ objectFit: "contain !important" }}>
+        <Carousel.Item>
+          <CarouselImageGenerator text="burger" />
+          <Carousel.Caption>
+            <div>
+              <Form className="d-flex mb-5 justify-content-center">
+                <Form.Control
+                  type="search"
+                  placeholder="Search your Delicious meal"
+                  className="me-2"
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                />
+              </Form>
+            </div>
+            <h3>Burger</h3>
+            <p>
+              TheBurger was first invented in SEYMOUR FAIR, WISCONSIN, in 1885
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <CarouselImageGenerator text="pizza" />
+          <Carousel.Caption>
+            <div>
+              <Form className="d-flex mb-5 justify-content-center">
+                <Form.Control
+                  type="search"
+                  placeholder="Search your Delicious meal"
+                  className="me-2"
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                />
+              </Form>
+            </div>
+            <h3>Pizza</h3>
+            <p>About 350 slices of Pizza are consumed every second in the US</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <CarouselImageGenerator text="taco" />
+          <Carousel.Caption>
+            <div>
+              <Form className="d-flex mb-5 justify-content-center">
+                <Form.Control
+                  type="search"
+                  placeholder="Search your Delicious meal"
+                  className="me-2"
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                />
+              </Form>
+            </div>
+            <h3>Taco</h3>
+            <p>
+              The biggest taco ever made was constructed on November 20th, 2011
+              in Queretaro, Mexico.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
       <div className="container">
-        {foodCat !== []
-          ? foodCat.map((data) => {
-              return (
-                <div className="row mb-3">
-                  <div key={data._id} className="fs-4 m-3">
-                    {data.CategoryName}
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          foodCat.map((data) => (
+            <div className="row mb-3" key={data._id}>
+              <div className="fs-4 m-3">{data.CategoryName}</div>
+              <hr />
+              {foodItem
+                .filter(
+                  (item) =>
+                    item.CategoryName === data.CategoryName &&
+                    item.name.toLowerCase().includes(search.toLocaleLowerCase())
+                )
+                .map((filterItems) => (
+                  <div
+                    key={filterItems._id}
+                    className="col-12 col-md-6 col-lg-3"
+                  >
+                    <NewCard
+                      foodName={filterItems.name}
+                      options={filterItems.options[0]}
+                      imgSrc={filterItems.img}
+                      description={filterItems.description}
+                    />
                   </div>
-                  <hr />
-                  {foodItem !== [] ? (
-                    foodItem
-                      .filter((item) => item.CategoryName === data.CategoryName)
-                      .map((filterItems) => {
-                        return (
-                          <div
-                            key={filterItems._id}
-                            className="col-12 col-md-6 col-lg-3"
-                          >
-                            <NewCard
-                              foodName={filterItems.name}
-                              options={filterItems.options}
-                              imgSrc={filterItems.img}
-                              description={filterItems.description}
-                            />
-                          </div>
-                        );
-                      })
-                  ) : (
-                    <div>No such Data is found</div>
-                  )}
-                </div>
-              );
-            })
-          : ""}
+                ))}
+            </div>
+          ))
+        )}
       </div>
-      <div>
-        <Footer />
-      </div>
+
+      <Footer />
     </div>
   );
 };
